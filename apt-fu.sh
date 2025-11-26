a25e9cf0879ac6cf3f96bb53146d160f331728ef
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2025-11-25 23:24:05 -0300

    W

--- apt-fu.sh
+++ apt-fu.sh
@@ -2,9 +2,11 @@
 #  --no-install-recommends --no-install-suggests
 sudo apt-get update --assume-yes \
 && sudo apt-get install --assume-yes ubuntu-desktop
-&& sudo rm -frv /var/cache/apt/archives/* \
+
+sudo rm -frv -- /var/cache/apt/archives/* \
 && sudo apt-get autoremove \
-&& echo 'If both are enable, disable one of them!' \
+
+echo 'If both are enable, disable one of them!' \
 && systemctl is-enabled NetworkManager-wait-online.service \
 && systemctl is-enabled systemd-networkd-wait-online.service \
 && systemctl disable systemd-networkd.service
