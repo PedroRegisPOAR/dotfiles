0ffc0fda23861a664371393252eccaf0b8cdbd5b
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2025-11-25 23:45:19 -0300

    W

--- apt-fu.sh
+++ apt-fu.sh
@@ -7,6 +7,6 @@ sudo rm -frv -- /var/cache/apt/archives/* \
 && sudo apt-get autoremove \
 
 echo 'If both are enable, disable one of them!' \
-&& systemctl is-enabled NetworkManager-wait-online.service \
-&& systemctl is-enabled systemd-networkd-wait-online.service \
-&& systemctl disable systemd-networkd.service
+&& sudo systemctl is-enabled NetworkManager-wait-online.service \
+&& sudo systemctl is-enabled systemd-networkd-wait-online.service \
+&& sudo systemctl disable systemd-networkd.service
