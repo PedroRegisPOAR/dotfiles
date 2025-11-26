bf72b0c9cf1e8a08bd9aed3f049ad381b928dcaf
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2025-11-25 23:45:49 -0300

    W

--- apt-fu.sh
+++ apt-fu.sh
@@ -4,7 +4,7 @@ sudo apt-get update --assume-yes \
 && sudo apt-get install --assume-yes ubuntu-desktop
 
 sudo rm -frv -- /var/cache/apt/archives/* \
-&& sudo apt-get autoremove \
+&& sudo apt-get autoremove
 
 echo 'If both are enable, disable one of them!' \
 && sudo systemctl is-enabled NetworkManager-wait-online.service \
