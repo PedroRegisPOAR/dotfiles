2fcbb30f7cdb170f33744bc4d4b3d930c927cabb
Pedro Regis <pedroregis@Mac.home>
2026-05-05 21:08:43 -0300

    WIP

--- src/templates/default.nix
+++ src/templates/default.nix
@@ -1,15 +1,6 @@
-    {
-    startConfigNixFlakesHomeManagerZsh = {
-      description = "Base configuration";
-      path = ./utm-nixos-bootstrap;
-      welcomeText = ''
-        # A
-        It was created to be an start point with some batteries include.
-        ## B
-        C
-          ## More D
-        - [nixos.wiki](https://nixos.wiki/wiki/)
-        - ...
-      '';
-    };
-    }
+{
+  UTMNixOSBootstap = {
+    description = "Base configuration";
+    path = ./utm-nixos-bootstrap;
+  };
+}
