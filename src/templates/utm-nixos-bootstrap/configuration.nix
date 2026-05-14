4ca57ca7688236cd900b827494f559df92ba4fe1
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2026-05-13 22:00:18 -0300

    w

--- src/templates/utm-nixos-bootstrap/configuration.nix
+++ src/templates/utm-nixos-bootstrap/configuration.nix
@@ -75,6 +75,18 @@
   # services.displayManager.autoLogin.user = "fog";
 
   services.spice-vdagentd.enable = true;
+  systemd.user.services.spice-vdagent = {
+    description = "spice-vdagent user daemon";
+    after = [ "spice-vdagentd.service" "graphical-session.target" ];
+    requires = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.spice-vdagent}/bin/spice-vdagent -x";
+    };
+    unitConfig = {
+      ConditionPathExists = "/run/spice-vdagentd/spice-vdagent-sock";
+    };
+  };
   services.xserver.videoDrivers = [ "qxl" ];
 
   # services.getty.helpLine = pkgs.lib.mkForce "" ; 
