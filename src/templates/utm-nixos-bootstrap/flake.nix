d78b66bee6be99993df9e6d8f255efcf6a0252d5
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2026-05-18 22:37:40 -0300

    Refactor flake.nix to support multiple systems and overlays

--- src/templates/utm-nixos-bootstrap/flake.nix
+++ src/templates/utm-nixos-bootstrap/flake.nix
@@ -1,28 +1,70 @@
 {
+
+  /*
+    nix \
+    flake \
+    lock \
+    --override-input nixpkgs 'github:NixOS/nixpkgs/d7a713c0b7e47c908258e71cba7a2d77cc8d71d5' 
+  */
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    # mcp-nixos.url = "github:utensils/mcp-nixos";
-    # mcp-nixos.nixpkgs.follows = "nixpkgs";
   };
 
-  # outputs = { self, nixpkgs, mcp-nixos, ... }:
   outputs = { self, nixpkgs, ... }:
-    {
-      formatter.aarch64-linux = nixpkgs.legacyPackages."aarch64-linux".nixpkgs-fmt;
-      nixosConfigurations.n1x0s = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          ./configuration.nix
+    let
+      systems = [ "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
 
-          # Provide an initial copy of the NixOS channel so that the user
-          # doesn't need to run "nix-channel --update" first.
-          # "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-
-          # For virtualisation settings.
-          # It brings among other things the `.vm` attr need for 
-          # "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
-        ];
+      overlays.default = final: prev: {
+        allTests = final.writeShellApplication
+          {
+            name = "all-tests";
+            runtimeInputs = [ ];
+            text = ''
+              sudo nix fmt . \
+              && nix flake show --all-systems '.#' \
+              && nix flake metadata '.#' \
+              && nix build --no-link --print-build-logs --print-out-paths '.#' \
+              && nix build --no-link --print-build-logs --print-out-paths --rebuild '.#' \
+              && nix develop '.#' --command sh -c 'true' \
+              && nix flake check --all-systems --verbose '.#'
+            '';
+          } // { meta.mainProgram = "all-tests"; };
       };
 
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        overlays = [ overlays.default ];
+      };
+    in
+    {
+      inherit overlays;
+
+      formatter = forAllSystems (system: (pkgsFor system).nixpkgs-fmt);
+
+      packages = forAllSystems (system:
+        let pkgs = pkgsFor system; in {
+          inherit (pkgs) allTests;
+          default = pkgs.allTests;
+        });
+
+      devShells = forAllSystems (system: {
+        default = (pkgsFor system).mkShell { };
+      });
+
+      nixosConfigurations =
+        let
+          mkNixos = system: nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              ./configuration.nix
+              { nixpkgs.overlays = [ overlays.default ]; }
+            ];
+          };
+        in
+        {
+          n1x0s = mkNixos "aarch64-linux";
+          n1x0s-x86 = mkNixos "x86_64-linux";
+        };
     };
 }
