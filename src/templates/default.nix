5b62e511ed432e39415deec911dae99b53b9bf0e
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2026-05-28 22:14:26 -0300

    feat: new template, after bootstrap adds tooling

--- src/templates/default.nix
+++ src/templates/default.nix
@@ -3,4 +3,9 @@
     description = "Base configuration";
     path = ./utm-nixos-bootstrap;
   };
+
+  UTMNixOS = {
+    description = "NixOS configuration with tools";
+    path = ./utm-nixos;
+  };
 }
